1edea86a3e4e09c27be641671ccbe5a25b9409e4
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-20 23:31:23 +0700

    Menyelesaikan masalah

--- stack.cpp
+++ stack.cpp
@@ -29,6 +29,10 @@ void pop(){
         if (isEmpty()){
           cout << "stack is empty." << endl;
           return;
+           node *temp = top;
+           top = top->next;  
+           cout << "Popped value: " << temp->data << endl;
+           delete temp;  
         }
        
 }
@@ -61,7 +65,7 @@ int main(){
   int choice = 0;
   int value;
 
-    while (choice != 5)
+    while (choice != 4)
     {
         cout << "1. Push\n";
         cout << "2. Pop\n";
