31fed8a7753c6e4bde407e8886c4f6923d17d626
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-20 23:18:00 +0700

    defaault choice

--- stack.cpp
+++ stack.cpp
@@ -86,6 +86,10 @@ int main(){
              case 4:
                 cout << "Exiting program." << endl;
                 break;
+             default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
+        }
 
         }
 
