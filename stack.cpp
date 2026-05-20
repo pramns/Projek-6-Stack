dbaf884ad10be117c2a7f4720f85f24f77ec9998
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-20 23:14:35 +0700

    create while with 5 option

--- stack.cpp
+++ stack.cpp
@@ -57,5 +57,19 @@ bool isEmpty()
 
 int main(){
   stack stack;
+
+  int choice = 0;
+  int value;
+
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
     return 0;
+}
 }
